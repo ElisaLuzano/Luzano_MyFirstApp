240469f2a65eb7edd47bdafc018d059539c2249e
ElisaLuzano <emqLuzano@mcm.edu.ph>
2023-03-07 18:20:43 +0800

    Luzano_MyFirstApp

---
+++ app/src/main/java/com/example/luzano_myfirstapp/Luzano_MyFirstApp.java
@@ -0,0 +1,13 @@
+package com.example.luzano_myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Luzano_MyFirstApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_luzano_my_first_app);
+    }
+}
